ecd1ca11ec1b5e936488f9ef4e36dcbba9d39bcb
snackdev <snackyoo0522@gmail.com>
2022-02-23 20:20:29 +0900

    Initial commit

---
+++ src/main/java/snackdev/kafka/bestpractice/BestpracticeApplication.java
@@ -0,0 +1,13 @@
+package snackdev.kafka.bestpractice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BestpracticeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BestpracticeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/snackdev/kafka/bestpractice/BestpracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package snackdev.kafka.bestpractice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BestpracticeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
